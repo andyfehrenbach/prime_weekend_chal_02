28b8ac98d12edd25077d6d3861ba8a78591a98d1
andyfehrenbach <Andy Fehrenbach@Andy-Fehrenbachs-MacBook-Pro-2.local>
2016-02-07 13:50:57 -0600

    reconfigured code and variable scope, and added timer.

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,44 +1,16 @@
-$(document).ready(function(){
-    var index = 0; //this will be used to track every classmate and act as an index for accessing JSON objects
+// Andy Fehrenbach - 2/7/2016
 
+var index = 0; //this will be used to track every classmate and act as an index for accessing JSON objects
+var timer;
+
+$(document).ready(function(){
     getData(index); //get Kris Szafranski's div to start the party
 
-     $('#next').on('click', function advanceCassette() {
-        index++;
+    timer = window.setInterval(advanceCassette, 5000);
 
-        //this bit allows the slider to loop continuously forwards and adjusts the index accordingly
-        // there's a bug when looping that selects the second index instead of the first
-        // would prefer that the 18 was not hard-coded...how to access array.length on a JSON Object?
-        if (index > 18) {
-            index = 0;
-        }
-        $('li.index-point').first().addClass('index-point-active');
-        $('li').last().removeClass('index-point-active');
 
-        //this will append the data for an individual at the given index, and remove the previous div
-        getData(index);
-        console.log(index);
-        $('#peopleContainer').children().first().remove();
-        $('li.index-point-active').next().addClass('index-point-active');  //advance the nav index--this is buggy
-        $('li.index-point-active').prev().removeClass('index-point-active');
-    });
-
-    // this function does the opposite of the previous function
-    $('#previous').on('click', function rewindCassette(){
-        index--;
-        if(index < 0) {
-            index = 18;
-        }
-            $('li.index-point').first().removeClass('index-point-active');
-            $('li').last().addClass('index-point-active');
-        console.log(index);
-        getData(index);
-        $('#peopleContainer').children().last().remove();
-        $('.index-point-active').prev().addClass('index-point-active');
-        $('.index-point-active').next().removeClass('index-point-active');
-    });
-   // window.setInterval(advanceCassette, 5000);
-
+     $('#next').on('click', advanceCassette); //yes, like the song by Spoon.
+    $('#previous').on('click', rewindCassette);
 });
 
 //end document ready function
@@ -56,22 +28,49 @@ function getData(counter){
                 $el.append('<h2>' + 'Favorite Movie #1: ' + data.people[counter].favoriteMovie1 + '</h2>');
                 $el.append('<h2>' + 'Favorite Movie #2: ' + data.people[counter].favoriteMovie2 + '</h2>');
                 $el.append('<h2>' + 'Favortie Song: ' + data.people[counter].favoriteSong + '</h2>');
-
-
-                //$el.data("classMateNumber", counterId);
-
-                //console.log($el.data("classMateNumber"));
-
-
-            //console.log(data);
         },
         error: function() {
             console.log('ERROR: Unable to contact the server.');
         }
-
     });
 }
 
 
+function advanceCassette() {
+    index++;
 
+    //this bit allows the slider to loop continuously forwards and adjusts the index accordingly
+    // there's a bug when looping that selects the second index instead of the first
+    // would prefer that the 18 was not hard-coded...how to access array.length on a JSON Object?
+    if (index > 18) {
+        index = 0;
+    }
+    $('li.index-point').first().addClass('index-point-active');
+    $('li').last().removeClass('index-point-active');
+
+    //this will append the data for an individual at the given index, and remove the previous div
+    getData(index);
+    console.log(index);
+    $('#peopleContainer').children().first().remove();
+    $('li.index-point-active').next().addClass('index-point-active');  //advance the nav index--this is buggy
+    $('li.index-point-active').prev().removeClass('index-point-active');
+
+
+
+
+}
+
+function rewindCassette() {
+    index--;
+    if(index < 0) {
+        index = 18;
+    }
+    $('li.index-point').first().removeClass('index-point-active');
+    $('li').last().addClass('index-point-active');
+    console.log(index);
+    getData(index);
+    $('#peopleContainer').children().last().remove();
+    $('.index-point-active').prev().addClass('index-point-active');
+    $('.index-point-active').next().removeClass('index-point-active');
+}
 
